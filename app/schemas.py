857082703cb7d0dd4f1866d2d60f2e083b57be8b
joffinkoshy <joffin2012@gmail.com>
2025-12-15 17:15:45 +0530

    testing

--- app/schemas.py
+++ app/schemas.py
@@ -7,7 +7,7 @@ class Candidate(BaseModel): # for one candidate
     skill_match_score:float
     interview_score:float
     salary_expectation:float
-    resume:str
+    resume_text:str
 
 class RankCandidatesRequest(BaseModel): # list of candidates each list applying for unique job_id
     job_id: str
